dec3539e485a0c71fc4733b7ef23230d70048267
francescopirox <f.pironti1999@gmail.com>
2024-09-21 12:48:01 +0200

    Added Point 3D

---
+++ java/oop/geom/v1/Point3D.java
@@ -0,0 +1,56 @@
+package oop.geom.v1;
+
+
+public class Point3D {
+    //Variabili d'istanza
+    private double x;
+    private double y;
+    private double z;
+
+    //Costruttore
+    public Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //Costruttore di Copia
+    public Point3D(Point3D p){
+        this.x = p.x;
+        this.y = p.y;
+        this.z = p.z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distance(Point3D p){
+        return Math.sqrt((p.x-x)*(p.x-x)+(p.y-y)*(p.y-y)+(p.z-z)*(p.z-z));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point3D point3D)) return false;
+        Point3D p =(Point3D) o;
+        return this.distance(p)<=Point2D.EPS;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
