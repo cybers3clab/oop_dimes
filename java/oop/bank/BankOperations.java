98240b93e979b7e60b23c72fdcc8e1ca782819ad
francescopirox <f.pironti1999@gmail.com>
2024-10-22 10:16:30 +0200

    Esercitazione 5 Update

--- java/oop/bank/BankOperations.java
+++ java/oop/bank/BankOperations.java
@@ -2,7 +2,7 @@ package oop.bank;
 
 public interface
 BankOperations extends Comparable<BankOperations>{
-    boolean deposita(double val) throws Exception;
+    boolean deposita(double val) throws NegativeFund;
     boolean preleva(double val) throws SaldoInsufficente;
     double saldo();
 
